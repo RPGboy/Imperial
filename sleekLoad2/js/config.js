5dbd00ec35bf0f6528aa15ed7bffc39f225162fb
RPGboy <34611274+RPGboy@users.noreply.github.com>
2017-12-25 15:37:49 +0000

    Update config.js

--- sleekLoad2/js/config.js
+++ sleekLoad2/js/config.js
@@ -18,7 +18,7 @@ var l_displayMapGamemode = true;
 
 /*Custom server name configuration
 Leave it blank for automatic server name*/
-var l_serverName = "sleekLoad 2";
+var l_serverName = "Polar Networks";
 
 /*Use a video for the background?
 Otherwise it will use images
